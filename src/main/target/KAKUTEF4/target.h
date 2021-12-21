cf4f93a0147fe95c9e988215d6b92ba719b2cc5f
konstantinfpv <84969930+konstantinfpv@users.noreply.github.com>
2021-12-22 02:00:14 +0300

    Update target.h

--- src/main/target/KAKUTEF4/target.h
+++ src/main/target/KAKUTEF4/target.h
@@ -97,8 +97,8 @@
 #define USE_UART_INVERTER
 
 #define USE_UART1
-#define UART1_RX_PIN            PA10
-#define UART1_TX_PIN            PA9
+#define UART1_RX_PIN            PB7
+#define UART1_TX_PIN            PB6
 
 #define USE_UART3
 #define UART3_RX_PIN            PB11
